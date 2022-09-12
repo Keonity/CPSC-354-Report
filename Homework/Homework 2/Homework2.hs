9c993a6a8014b3c0f31eaf795458e9e6299d1ab0
Keoni Lanoza <lanoza@chapman.edu>
2022-09-11 17:51:03 -0700

    Homework 2

---
+++ Homework/Homework 2/Homework2.hs
@@ -0,0 +1,29 @@
+import Data.List
+import System.IO
+
+select_evens :: [Int] -> [Int]
+select_evens (x:xs) = [(x:xs)!!y | y <- (y:ys)] 
+  where (y:ys) = [1,3..(length (x:xs)-1)]
+
+select_odds :: [Int] -> [Int]
+select_odds (x:xs) = [(x:xs)!!y | y <- (y:ys)] 
+  where (y:ys) = [0,2..(length (x:xs)-1)]
+
+member :: Int -> [Int] -> Bool
+member _ _ = False
+member x (y:ys)
+  | x == y = True
+  | otherwise = member x ys
+
+append :: [Int] -> [Int] -> [Int]
+append (x:xs) (y:ys) = x:xs ++ y:ys
+
+revert :: [Int] -> [Int]
+revert [] = []
+revert (x:xs) = revert (xs) ++ [x]
+
+less_equal :: [Int] -> [Int] -> Bool
+less_equal (x:xs) (y:ys)
+  | x >= y = False
+  | length (xs) == 0 && length (ys) == 0 = True
+  | otherwise = less_equal xs ys
