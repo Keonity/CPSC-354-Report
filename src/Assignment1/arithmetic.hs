7bc363aa789f4e2821c8e258a877082eddebe74c
Keoni Lanoza <lanoza@chapman.edu>
2022-09-27 15:51:09 -0700

    Pushing subN and divN functions for feedback
    subN and divN are incomplete but are being pushed in order to ask questions

--- src/Assignment1/arithmetic.hs
+++ src/Assignment1/arithmetic.hs
@@ -6,6 +6,9 @@
 -----------------------
 
 -- Natural numbers
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 data NN = O | S NN
   deriving (Eq,Show) -- for equality and printing
 
@@ -36,8 +39,8 @@ addP (T m) (T n) = T (T (addP m n))
 
 multP :: PP -> PP -> PP
 multP I I = I
-multP I (T m) = addP (multP I m) (m)
-multP (T m) I = addP (multP I m) (m)
+multP I (T m) = addP (multP I m) m
+multP (T m) I = addP (multP I m) m
 multP (T m) (T n) = T (T (multP m (T n)))
 
 nn_pp :: PP -> NN
@@ -60,6 +63,29 @@ addN (S n) m = S (addN n m)
 multN :: NN -> NN -> NN
 multN O m = O
 multN (S n) m = addN (multN n m) m
+
+subN :: NN -> PP -> NN
+subN O m = O
+subN (S n) I = n
+subN (S n) (T m) = subN n m
+-- subN 7 4 = subN 6 3
+-- subN 6 3 = subN 5 2
+-- subN 5 2 = subN 4 1
+-- subN 4 1 = 3
+
+divN :: NN -> PP -> NN
+divN O m = O
+divN (S m) I = S m
+divN (S O) m = S O
+divN (S m) n = divN (subN (S m) n) n
+-- divN 7 4 = divN (subN 7 4) 4
+-- divN (subN 7 4) 4 = divN (subN 6 3) 3
+-- divN (subN 6 3) 3 = divN (subN 5 2) 2
+-- divN (subN 5 2) 2 = divN (subN 4 1) 1
+-- divN (subN 4 1) 1 = subN 4 1
+-- subN 4 1 = 3
+
+-- How to prevent divN from making (S n) go to zero?
 
 ----------------
 -- II Arithmetic
@@ -87,17 +113,32 @@ multN (S n) m = addN (multN n m) m
 main = do
     print $ addN (S (S O)) (S O) -- S (S (S O))
     print $ multN (S (S O)) (S (S (S O))) -- S (S (S (S (S (S O)))))
-    print $ addP (I) (I) -- (T I)
-    print $ addP (T I) (I) -- T (T I)
-    print $ addP (I) (T I) -- T (T I)
+
+    print $ addP I I -- (T I)
+    print $ addP (T I) I -- T (T I)
+    print $ addP I (T I) -- T (T I)
     print $ addP (T (T I)) (T I) -- T (T (T (T I))))
+    
     print $ multP I I -- I
     print $ multP I (T I) -- (T I)
     print $ multP (T I) I -- (T I)
     print $ multP (T I) (T I) -- T (T (T I))
     print $ multP (T (T I)) (T I) -- T (T (T (T (T I))))
     print $ multP (T I) (T (T I)) -- T (T (T (T (T I))))
+    
     print $ nn_pp I -- S (O)
     print $ nn_pp (T I) -- S (S O)
+    
     print $ ii_pp I -- II (S O) O
     print $ ii_pp (T I) -- II (S (S O)) O
+    
+    print $ subN (S (S O)) I -- S O
+    print $ subN (S (S O)) (T I) -- O
+    print $ subN (S (S (S (S (S O))))) (T (T I)) -- S (S O)
+    print $ subN (S (S (S (S (S (S (S O))))))) (T (T (T I))) -- S (S (S O))
+
+    print $ divN (S (S O)) I -- S (S O)
+    print $ divN (S (S (S (S (S (S (S O))))))) (T (T (T I))) -- S (S (S O))
+    print $ divN (S O) I -- S O
+    
+    
