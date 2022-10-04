7197c3bed3429d2dc54e9e22d01352e19ff0e667
Keoni Lanoza <56704285+Keonity@users.noreply.github.com>
2022-10-03 23:44:41 -0700

    Extended numbers.cf CFG and Interpreter.hs eval function

--- Assignment1/Part 2/Interpreter.hs
+++ Assignment1/Part 2/Interpreter.hs
@@ -1,8 +1,19 @@
 module Interpreter where
 
 import AbsNumbers
+    ( Exp(..) )
 
 eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
+eval (Minus n m) = (eval n) - (eval m)
 eval (Times n m) = (eval n) * (eval m)
+eval (IntDivision n m) = div (eval n) (eval m) 
+eval (Modulus n m) = mod (eval n) (eval m)
+eval (Negative n) = -(eval n)
+eval (Exponent n m) = (eval n) ^ (eval m)
+eval (Paranth n) = (eval n)
+
+main = do
+    print "Num"
+    print $ eval(Plus (Num 2) (Num 3))
