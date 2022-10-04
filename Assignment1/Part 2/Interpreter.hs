c733d482387dedc39d86b4bfe1aa0cc47040e72a
Keoni Lanoza <56704285+Keonity@users.noreply.github.com>
2022-10-03 23:50:07 -0700

    Added test cases (All successful and match with CFG)

--- Assignment1/Part 2/Interpreter.hs
+++ Assignment1/Part 2/Interpreter.hs
@@ -16,4 +16,34 @@ eval (Paranth n) = (eval n)
 
 main = do
     print "Num"
+    print $ eval(Num 1)
+
+    print "Plus"
     print $ eval(Plus (Num 2) (Num 3))
+
+    print "Minus"
+    print $ eval(Minus (Num 2) (Num 3))
+
+    print "Times"
+    print $ eval(Times (Num 2) (Num 3))
+
+    print "Integer Division"
+    print $ eval(IntDivision (Num 2) (Num 3))    
+
+    print "Modulus"
+    print $ eval(Modulus (Num 2) (Num 3)) 
+
+    print "Negative"
+    print $ eval(Negative (Num 1))
+
+    print "Exponent"
+    print $ eval(Exponent (Num 2) (Num 3))    
+
+    print "Parantheses"
+    print $ eval(Plus (Num 1) (Paranth (Plus(Num 2) (Num 3))))
+
+    print "Instructor-Given Test Cases"
+    print $ eval(Plus (Paranth (Exponent (Num 2) (Num 3))) (Num 4))
+
+    print "Instructor-Given Test Cases"
+    print $ eval(IntDivision (Paranth (Exponent (Num 2) (Num 3))) (Paranth (Exponent (Num 4) (Num 5))))
