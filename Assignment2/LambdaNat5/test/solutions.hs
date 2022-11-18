3eadf0c1b506c9f73dd57b9e755471022678e176
Keoni Lanoza <56704285+Keonity@users.noreply.github.com>
2022-11-17 18:40:26 -0800

    Added more test cases to solutions.hs

--- Assignment2/LambdaNat5/test/solutions.hs
+++ Assignment2/LambdaNat5/test/solutions.hs
@@ -3,7 +3,7 @@
 module Main where
 import Prelude hiding (reverse, even, length)
 
-is_empty :: [a] -> Bool
+is_empty :: [Integer] -> Bool
 is_empty [] = True
 is_empty a = False
 
@@ -14,17 +14,17 @@ fib a =
     else if a == 1 then 1
     else fib(a-1) + fib(a-2)
 
-length :: [a] -> Integer
+length :: [Integer] -> Integer
 length [] = 0
 length a = 1 + length(tail a)
 
-even :: [a] -> Bool
+even :: [Integer] -> Bool
 even [] = False
 even a = 
     if mod (length(a)) (2) == 0 then True
     else False
 
-reverse :: [a] -> [a]
+reverse :: [Integer] -> [Integer]
 reverse [] = []
 reverse (x:xs) = (reverse (xs)) ++ [x]
 
@@ -42,8 +42,15 @@ main = do
     print $ is_empty [1,2,3]
     print $ is_empty [-1,-2,-3]
     print $ fib 14
+    print $ fib (-14)
     print $ length [1,2,3]
+    print $ length [-1,-2,-3]
+    print $ length []
     print $ even [1,2,3]
     print $ even [1,2,3,4]
     print $ reverse [1,2,3,4]
+    print $ reverse []
     print $ weave [0,1,4] [2,3,5]
+    print $ weave [2,3,5] [0,1,4]
+    print $ weave [-1,5,0] []
+    print $ weave [5,-1,0] []
