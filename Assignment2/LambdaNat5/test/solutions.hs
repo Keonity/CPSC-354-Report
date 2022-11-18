d654156c11bb4b6b6cbf72ec53aa034d5001ad9b
Keoni Lanoza <56704285+Keonity@users.noreply.github.com>
2022-11-17 18:08:32 -0800

    Added length function to solutions.hs

--- Assignment2/LambdaNat5/test/solutions.hs
+++ Assignment2/LambdaNat5/test/solutions.hs
@@ -1,15 +1,27 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 module Main where
+import Prelude hiding (length)
 
 is_empty :: [a] -> Bool
 is_empty [] = True
 is_empty a = False
 
+fib :: Integer -> Integer
+fib a =
+    if a <= 0 then 0
+    else if a == 0 then 0
+    else if a == 1 then 1
+    else fib(a-1) + fib(a-2)
 
+length :: [a] -> Integer
+length [] = 0
+length a = 1 + length(tail a)
 
 main = do
     --putStrLn ""
     print $ is_empty [] 
     print $ is_empty [1,2,3]
     print $ is_empty [-1,-2,-3]
+    print $ fib 14
+    print $ length [1,2,3]
